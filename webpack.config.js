326bdbce2719c23b99d0c27d18d4da768b63a5ee
Ahmad <ehmaddd@gmail.com>
2023-03-28 22:26:12 +0500

    Add supportive files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
