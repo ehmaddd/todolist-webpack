eaaf9e21caa48e41ad5f9a3e6acca7df398363a3
Ahmad <ehmaddd@gmail.com>
2023-03-30 13:03:52 +0500

    Add updated files

--- src/check.js
+++
@@ -1,6 +0,0 @@
-const checkBox = (checkId) => {
-  const testTask = document.querySelectorAll('.test-task');
-  testTask[checkId].style.textDecoration = 'line-through';
-};
-
-export default checkBox;
--- src/uncheck.js
+++
@@ -1,6 +0,0 @@
-const uncheckBox = (checkId) => {
-  const testTask = document.querySelectorAll('.test-task');
-  testTask[checkId].style.textDecoration = null;
-};
-
-export default uncheckBox;
